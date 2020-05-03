045ceac84cb6ac97ab5536bb918cdb007f44ab14
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-03 21:07:53 +0300

    create new files
    about.php
    header.php
    footer.php

---
+++ about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>TravelStates.com</title>
+</head>
+
+<body>
+    <?php require "header.php" ?>
+    <?php require "footer.php" ?>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -10,30 +10,21 @@
 </head>
 
 <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">TravelStates.com</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="#">Главная</a>
-            <a class="p-2 text-dark" href="#">Контакты</a>
-            <a class="p-2 text-dark" href="#">Контакты</a>
-            <a class="p-2 text-dark" href="#">Контакты</a>
-            <a class="p-2 text-dark" href="#">Контакты</a>
-            <a class="p-2 text-dark" href="#">Контакты</a>
-        </nav>
-        <a class="btn btn-outline-primary" href="#">Войти</a>
-    </div>
+    <?php require "header.php" ?>
     <div class="container mt-5">
         <h3 class="mb-5">States to visit</h3>
 
         <div class="d-flex flex-wrap">
             <?php
-            for ($i = 0; $i < 5; $i++) :
+            for ($i = 0; $i < 6; $i++) :
             ?>
                 <div class="card mb-4 shadow-sm">
                     <div class="card-header">
                         <h4 class="my-0 font-weight-normal">State</h4>
                     </div>
                     <div class="card-body">
+                        <img src="img/<?php echo ($i + 1) ?>.jpg" class="img-thumbnail">
+                        <!--тут увеличивае $i на 1раз -->
                         <ul class="list-unstyled mt-3 mb-4">
                             <li>Population:</li>
                             <li>Temperature:</li>
@@ -44,8 +35,9 @@
                 </div>
             <?php endfor; ?>
         </div>
-
     </div>
+    <?php require "footer.php" ?>
+
 </body>
 
 </html>

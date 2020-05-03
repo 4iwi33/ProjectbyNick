bedba4114b46d116a700f3e10e24fb4d3cd58964
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-03 22:25:04 +0300

    updt

--- check.php
+++ check.php
@@ -1,3 +1,24 @@
 <?php
-print_r($_POST); // принимаем метод POST
-?>
+// print_r($_POST); // принимаем метод POST
+$email = $_POST['email']; // из массива $_POST по ключю email записываем значение
+$message = $_POST['message'];
+// проверка 
+
+$error = ''; //запсиваем ошибки в переменную
+if (trim($email) == '') //если $email равен пустоте  
+    $error = 'Enter you email!'; // то выводим это
+
+else if (trim($message) == '') // если $message равен пустоте 
+    $error = 'Enter you message!'; // то выводим это
+
+else if (strlen($message) < 10) //если $message меньше 10 символов
+    $error = 'The message must be more 10 symbols!'; // то выводим это
+
+if ($error != '') { // если $error не равно пустоте 
+    echo $error;    // то вывести $error 
+    exit; // дальше ничего не обрабатывается
+}
+
+// mail — Отправляет электронную почту
+// strlen — Возвращает длину строки
+// trim - удаляет пробелы 

6bece9f27ae67bee7ae965dd2875d7b19ecbe7c8
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-06 20:21:54 +0300

    add button

--- header.php
+++ header.php
@@ -8,5 +8,12 @@
         <a class="p-2 text-dark" href="#">Контакты</a>
         <a class="p-2 text-dark" href="#">Контакты</a>
     </nav>
-    <a class="btn btn-outline-primary" href="#">Войти</a>
+    <?php
+    if ($_COOKIE['user'] == 'Yes') : // если куки["юзер"] равно Yes то вывести Kabint
+    ?>
+        <a class="btn btn-outline-primary" href="/ kabinet.php">Kabinet</a>
+    <?php else : ?>
+        <!-- иначе вывести Sign in -->
+        <a class="btn btn-outline-primary" href="/ kabinet.php">Sign in</a>
+    <?php endif; ?>
 </div>

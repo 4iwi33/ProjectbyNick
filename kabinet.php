94699f345d3e3f88b7faf6f8a699b81134496520
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-06 21:01:39 +0300

    Update kabinet.php

--- kabinet.php
+++ kabinet.php
@@ -3,6 +3,7 @@ if ($_COOKIE['user'] == 'Yes')  // если куки установлен
     setcookie('user', 'Yes', time() - 3600, '/'); // то от time отнимаем -3600
 else  
     setcookie('user', 'Yes', time() + 3600, '/'); // то устанавливаем time если куки не установлен
+
 header("Location: /");
 // "/" куки работает на всех страницах
 //  ".projectbynick.herokuapp.com" название моего домена

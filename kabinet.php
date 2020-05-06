119373a42217ab9a59fe934b23bb99798c639781
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-06 20:47:26 +0300

    add Sign && Kabinet

--- kabinet.php
+++ kabinet.php
@@ -1,5 +1,8 @@
 <?php
-setcookie('user', 'Yes', time() +3600, '/');
+if ($_COOKIE['user'] == 'Yes')  // если куки установлен 
+    setcookie('user', 'Yes', time() - 3600, '/'); // то куки -3600
+else
+    setcookie('user', 'Yes', time() + 3600, '/');
 header("Location: /");
 // "/" куки работает на всех страницах
 //  ".projectbynick.herokuapp.com" название моего домена
